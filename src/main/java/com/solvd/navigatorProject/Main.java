58ebe96e88ae1bf5e73f7dbad77906c0927d726e
aixauno <aixadolz@gmail.com>
2022-02-04 02:21:15 -0300

    updated

--- src/main/java/com/solvd/navigatorProject/Main.java
+++ src/main/java/com/solvd/navigatorProject/Main.java
@@ -4,7 +4,7 @@ import com.solvd.navigatorProject.binary.Node;
 import com.solvd.navigatorProject.exceptions.NoNodesAdded;
 import com.solvd.navigatorProject.exceptions.NoShortestPath;
 import com.solvd.navigatorProject.services.myBatisImpl.NodeServiceImpl;
-import com.solvd.navigatorProject.services.myBatisImpl.VertexConnectNodesServicesImpl;
+import com.solvd.navigatorProject.services.myBatisImpl.VertexServiceImpl;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
@@ -16,10 +16,10 @@ import java.util.Scanner;
 
 public class Main {
     private static final Logger log = LogManager.getLogger(Main.class);
-    private static NodeServiceImpl nodeService = new NodeServiceImpl();
-    private static VertexConnectNodesServicesImpl nhs = new VertexConnectNodesServicesImpl();
+    private static final NodeServiceImpl nodeService = new NodeServiceImpl();
+    private static final VertexServiceImpl vertexService = new VertexServiceImpl();
     private static final int INF = 999999999;
-    private static long[] amountOfNodes;
+    private static long[] amountOfVertices;
     private static long[][] initialMatrix;
     private static long[][] finalMatrix;
 
@@ -48,12 +48,12 @@ public class Main {
 
     public static void createInitialMatrix() {
 
-        amountOfNodes = new Random().longs(0, nodeService.getAll().size()).distinct().limit(6).toArray();
+        amountOfVertices = new Random().longs(0, vertexService.getAll().size()).distinct().limit(6).toArray();
         int i;
         int j;
 
-        for (i = 0; i < amountOfNodes.length; i++) {
-            for (j = 0; j < amountOfNodes.length; j++) {
+        for (i = 0; i < amountOfVertices.length; i++) {
+            for (j = 0; j < amountOfVertices.length; j++) {
                 System.out.print(initialMatrix[i][j] + " ");
             }
         }
@@ -62,12 +62,12 @@ public class Main {
 
     public static void findShortestPath() {
 
-        for (int i = 0; i < amountOfNodes.length; i++) {
-            for (int j = 0; j < amountOfNodes.length; j++) {
+        for (int i = 0; i < vertexService.getDistanceById(i); i++) {
+            for (int j = 0; j < vertexService.getDistanceById(j); j++) {
                 finalMatrix[i][j] = initialMatrix[i][j];
-                for (int k = 0; k < finalMatrix.length; k++) {
-                    for (i = 0; i < finalMatrix.length; i++) {
-                        for (j = 0; j < finalMatrix.length; j++) {
+                for (int k = 0; k < vertexService.getDistanceById(k); k++) {
+                    for (i = 0; i < vertexService.getDistanceById(i); i++) {
+                        for (j = 0; j < vertexService.getDistanceById(j); j++) {
                             if (finalMatrix[i][k] + finalMatrix[k][j] < finalMatrix[i][j])
                                 finalMatrix[i][j] = finalMatrix[i][k] + finalMatrix[k][j];
                         }
@@ -86,8 +86,8 @@ public class Main {
 
     public static void createFinalMatrix() {
         log.info('\n' + "Shortest distance matrix is: ");
-        for (int i = 0; i < amountOfNodes.length; ++i) {
-            for (int j = 0; j < amountOfNodes.length; ++j) {
+        for (int i = 0; i < vertexService.getDistanceById(i); ++i) {
+            for (int j = 0; j < vertexService.getDistanceById(j); ++j) {
                 if (finalMatrix[i][j] == INF) {
                     System.out.print("INF ");
                 } else {
@@ -97,6 +97,22 @@ public class Main {
             }
             System.out.println();
         }
+    }
+
+    public static long[][] getInitialMatrix() {
+        return initialMatrix;
+    }
+
+    public static void setInitialMatrix(long[][] initialMatrix) {
+        Main.initialMatrix = initialMatrix;
+    }
+
+    public static long[][] getFinalMatrix() {
+        return finalMatrix;
+    }
+
+    public static void setFinalMatrix(long[][] finalMatrix) {
+        Main.finalMatrix = finalMatrix;
     }
 }
 
