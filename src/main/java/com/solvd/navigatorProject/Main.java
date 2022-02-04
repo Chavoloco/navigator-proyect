86b84f67a4271372382a37c00a80bc167dd7aaf8
aixauno <aixadolz@gmail.com>
2022-02-04 01:52:16 -0300

    update

--- src/main/java/com/solvd/navigatorProject/Main.java
+++ src/main/java/com/solvd/navigatorProject/Main.java
@@ -1,100 +1,95 @@
 package com.solvd.navigatorProject;
 
-import com.solvd.navigatorProject.binary.Vertex;
+import com.solvd.navigatorProject.binary.Node;
 import com.solvd.navigatorProject.services.myBatisImpl.NodeServiceImpl;
 import com.solvd.navigatorProject.services.myBatisImpl.VertexConnectNodesServicesImpl;
-import com.solvd.navigatorProject.services.myBatisImpl.VertexServiceImpl;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.util.Objects;
+import java.util.Locale;
+import java.util.Random;
+import java.util.Scanner;
 
 
 public class Main {
     private static final Logger log = LogManager.getLogger(Main.class);
-    private static VertexServiceImpl vertexService = new VertexServiceImpl();
     private static NodeServiceImpl nodeService = new NodeServiceImpl();
     private static VertexConnectNodesServicesImpl nhs = new VertexConnectNodesServicesImpl();
-    private static final int numberOfVertices = 4;
     private static final int INF = 999999999;
-    private static long[][] finalMatrix = new long[numberOfVertices][numberOfVertices];
+    private static long[] amountOfNodes;
+    private static long[][] initialMatrix;
+    private static long[][] finalMatrix;
+
 
     public static void main(String[] args) {
 
+
         createInitialMatrix();
+
+
+    }
+
+    public static void askAmountOfNodes() {
+        Scanner scanner = new Scanner(System.in);
+
+        log.info("Add nodes writing their name. Type exit to stop.");
+        while (!scanner.nextLine().toUpperCase(Locale.ROOT).equals("EXIT")) {
+            String nodeName = scanner.nextLine();
+            nodeService.save(new Node(nodeName));
+        }
     }
 
     public static void createInitialMatrix() {
-        long matrix[][] = new long[numberOfVertices][numberOfVertices];
-        long first = nodeService.findFist().getId();
-        int column = 0;
-        int line = -1;
-        for (int i = (int) first; i < first + numberOfVertices; i++) {
-            line++;
-            for (int j = (int) first; j < first + numberOfVertices; j++) {
-                if (!Objects.equals(nodeService.getById(i), nodeService.getById(j))) {
-                    for (Vertex vertex : nhs.getVerticesByNodeId(i)) {
-                        if (nhs.getVerticesByNodeId(j).contains(vertex)) {
-                            matrix[line][column] = vertex.getDistance();
-                            break;
-                        } else {
-                            matrix[line][column] = INF;
-                        }
-                    }
-                    column++;
-                } else if (Objects.equals(nodeService.getById(i), nodeService.getById(j))) {
-                    matrix[line][column] = 0;
-                    column++;
-                }
+
+        amountOfNodes = new Random().longs(0, nodeService.getAll().size()).distinct().limit(6).toArray();
+        initialMatrix = new long[0][];
+        int i;
+        int j;
+        int k;
+
+        for (i = 0; i < amountOfNodes.length; i++) {
+            for (j = 0; j < amountOfNodes.length; j++) {
+                System.out.print(initialMatrix[i][j] + " ");
             }
-            column = 0;
         }
-
-
-        for (int i = 0; i < matrix.length; ++i) {
-            for (int j = 0; j < matrix.length; ++j) {
-
-                if (matrix[i][j] == INF)
-                    System.out.print("INF ");
-                else
-                    System.out.print(matrix[i][j] + "   ");
-            }
-            System.out.println();
-        }
-
-        floydAlgorithm(matrix);
-
+        System.out.println();
     }
 
-    public static void floydAlgorithm(long[][] matrix) {
-        long shortestDistance = 0;
+    public static void findShortestPath() {
 
-        for (int i = 0; i < numberOfVertices; i++)
-            for (int j = 0; j < numberOfVertices; j++)
-                finalMatrix[i][j] = matrix[i][j];
-        for (int k = 0; k < matrix.length; k++) {
-            for (int i = 0; i < matrix.length; i++) {
-                for (int j = 0; j < matrix.length; j++) {
-                    if (finalMatrix[i][k] + finalMatrix[k][j] < finalMatrix[i][j])
-                        finalMatrix[i][j] = finalMatrix[i][k] + finalMatrix[k][j];
-                    shortestDistance += finalMatrix[i][j];
+        for (int i = 0; i < amountOfNodes.length; i++) {
+            for (int j = 0; j < amountOfNodes.length; j++) {
+                finalMatrix = new long[0][];
+                finalMatrix[i][j] = initialMatrix[i][j];
+                for (int k = 0; k < finalMatrix.length; k++) {
+                    for (i = 0; i < finalMatrix.length; i++) {
+                        for (j = 0; j < finalMatrix.length; j++) {
+                            if (finalMatrix[i][k] + finalMatrix[k][j] < finalMatrix[i][j])
+                                finalMatrix[i][j] = finalMatrix[i][k] + finalMatrix[k][j];
+                        }
+                    }
                 }
             }
         }
-        createFinalMatrix();
     }
 
     public static void createFinalMatrix() {
         log.info('\n' + "Shortest distance matrix is: ");
-        for (int i = 0; i < numberOfVertices; ++i) {
-            for (int j = 0; j < numberOfVertices; ++j) {
-                if (finalMatrix[i][j] == INF)
+        for (int i = 0; i < amountOfNodes.length; ++i) {
+            for (int j = 0; j < amountOfNodes.length; ++j) {
+                if (finalMatrix[i][j] == INF) {
                     System.out.print("INF ");
-                else
+                } else {
                     System.out.print(finalMatrix[i][j] + "   ");
+                }
+                System.out.println();
             }
             System.out.println();
         }
     }
-
 }
+
+
+
+
+
